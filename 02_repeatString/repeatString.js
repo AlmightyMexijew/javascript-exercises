41ba9bf260e24d24888a86de931449084b57dd78
almightymexijew <ctepper625@gmail.com>
2023-09-13 17:41:28 +0300

    new stuff

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+   let mystring = String(string);
+   let mynum = Number(num);
+   if(mynum == 0){return ''};
+   if(mynum < 0){return 'ERROR'};
+   for(i=1;i<mynum;i++){mystring+=string};
+   return mystring;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let initialInput = String(input);
+    let myarr = []
+    let arr2 = []
+    myarr = initialInput.split('');
+    for(let i=0;i<myarr.length;i++){
+        arr2.unshift(myarr[i]);
+    };
+    arr2 = arr2.join('');
+    return arr2;
 };
 
 // Do not edit below this line
